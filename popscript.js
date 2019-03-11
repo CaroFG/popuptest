00d0a9868ea22a6ac2d5cf9758ae1a65e14993fc
CaroFG <48251481+CaroFG@users.noreply.github.com>
2019-03-11 15:32:00 +0100

    buttons work

--- popscript.js
+++ popscript.js
@@ -4,21 +4,25 @@ var modal = document.getElementById('myModal');
 
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var btn = document.getElementsByClassName("reject")[0];
 
-var btn = document.getElementById("confirm");
-var btn2 = document.getElementById("reject");
+
+
 // When the user clicks the button, open the modal
 var myVar = setTimeout(myFunction, 3000)
 
 
 function myFunction() {
   modal.style.display = "block";
-}
+};
+
+
+
+
 
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+btn.onclick = function() {
   modal.style.display = "none";
 }
 
