a6197b9e9779ce4e72437760002f61720a0312ae
CaroFG <48251481+CaroFG@users.noreply.github.com>
2019-03-11 14:44:20 +0100

    Add files via upload
    timer

--- popscript.js
+++ popscript.js
@@ -1,15 +1,13 @@
 // Get the modal
 var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
-
-myFunction();
+var myVar = setTimeout(myFunction, 3000);
 
 function myFunction() {
   modal.style.display = "block";
